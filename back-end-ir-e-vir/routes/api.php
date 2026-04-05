ac9646fd26fd56db1f0b95c7c16e4a57ddd671e6
Artthur2006 <arthursouza683107@gmail.com>
2026-04-05 13:24:52 -0300

    feat: implement parking API routes (entrance, exit, permanence)

--- back-end-ir-e-vir/routes/api.php
+++ back-end-ir-e-vir/routes/api.php
@@ -2,7 +2,18 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\EstacionamentoController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::prefix('estacionamento')->group(function () {
+
+    Route::post('/entrada', [EstacionamentoController::class, 'entrada']);
+
+    Route::post('/saida', [EstacionamentoController::class, 'saida']);
+
+    Route::get('/permanencias/{placa}', [EstacionamentoController::class, 'listar']);
+
+});
